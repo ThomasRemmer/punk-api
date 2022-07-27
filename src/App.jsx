acf963dfd0f84661fcf8355dd51495134cf60692
Thomas Remmer <remmert765@gmail.com>
2022-07-27 15:59:13 +0100

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import logo from './logo.svg';
+import {setState} from "react";
 import './App.css';
 
-function App() {
+const [beers,setBeers] = setState("")
+
+const getBeers = async () => {
+  const url = "https://api.punkapi.com/v2/beers/";
+  const res = await fetch(url);
+  const data = await res.json();
+  getBeers(data.results);
+};
+
+const App =() => {
   return (
       <h1>hello word</h1>
   );
