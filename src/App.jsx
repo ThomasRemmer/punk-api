c2f8679c0d4c7788f74e8d42a2e86f8ca7551410
Thomas Remmer <remmert765@gmail.com>
2022-07-27 17:20:34 +0100

    fixed api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,65 @@
-import {setState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import './App.scss';
 import NavContainer from "./Containers/NavContainer/NavContainer";
 import CardContainer from "./Containers/CardContainer/CardContainer";
-const [beers,setBeers] = setState("")
-const [ph,setPh] = setState(false)
-const [classic,setClassic] = setState("")
-const [abv,setAbv] = setState("")
-const [input, setInput] =setState("")
 
-const getBeers = async () => {
-  const url = "https://api.punkapi.com/v2/beers/";
-  const res = await fetch(url);
-  const data = await res.json();
-  setBeers(data.results);
-};
-
-// useEffect(() => { getBeers(); }, [ph, classic, abv, input]);
-
-const handlePh = (e) => {
-  if (e.target.checked === true){
-    setPh(true)
-  } else {
-    setPh(false)
-  }
-}
-
-const handleAbv = (e) => {
-  if (e.target.checked === true){
-    setAbv("abv_gt=6")
-  }
-}
-
-const handleClassic = (e) => {
-  if (e.target.checked === true){
-    setClassic("brewed_beffore=01/2010")
-  }
-}
-
-const handleInput = (e) => {
-  if (e.target.value !== "") {
-    setInput(`beer_name=${e.target.value}`)
-  }
-}
 
 const App =() => {
+
+  const [beers,setBeers] = useState([])
+  const [ph,setPh] = useState(false)
+  const [classic,setClassic] = useState("")
+  const [abv,setAbv] = useState("")
+  const [input, setInput] =useState("")
+  
+  const getBeers = async () => {
+    // let query =`${classic}&${abv}&${input}`
+    const url = `https://api.punkapi.com/v2/beers/`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setBeers(data);
+  
+  };
+  
+  useEffect(() => {
+    getBeers(ph, classic, abv, input);
+  }, [ph, classic, abv, input]);
+  
+  
+  // const handlePh = (e) => {
+  //   if (e.target.checked === true){
+  //     setPh(true)
+  //   } else {
+  //     setPh(false)
+  //   }
+  // }
+  
+  // const handleAbv = (e) => {
+  //   if (e.target.checked === true){
+  //     setAbv("abv_gt=6")
+  //   }
+  // }
+  
+  // const handleClassic = (e) => {
+  //   if (e.target.checked === true){
+  //     setClassic("brewed_beffore=01/2010")
+  //   }
+  // }
+  
+  // const handleInput = (e) => {
+  //   if (e.target.value !== "") {
+  //     setInput(`beer_name=${e.target.value}`)
+  //   }
+  // }
+  
+  console.log(beers)
   return (
       <>
       <nav>
-        <NavContainer />
+        
       </nav>
       <main>
-        <CardContainer beers={beers}/>
+        
       </main>
       </>
   );
--- src/Containers/CardContainer/CardContainer.jsx
+++ src/Containers/CardContainer/CardContainer.jsx
@@ -2,14 +2,21 @@ import Card from "../../Components/Card/Card"
 
 const CardContainer = ({beers}) => {
 
-    const cards = beers.map((beer) => {
-        return ( <div>
-            <Card id={beer.id} name={beer.name} tag={beer.tagline} desc={beer.description} img={beer.img_url} abv={beer.abv} ph={beer.ph} date={beer.first_brewed} />
-        </div>)
-    })
 
     return (
-        <div>{cards}</div>
+        <div className="profile-container">
+            {beers.map((beer) => (
+            <Card 
+                id={beer.id} 
+                name={beer.name} 
+                tag={beer.tagline} 
+                desc={beer.description} 
+                img={beer.img_url} 
+                abv={beer.abv} 
+                ph={beer.ph} 
+                date={beer.first_brewed} />
+            ))}
+        </div>
     )
 }
 
